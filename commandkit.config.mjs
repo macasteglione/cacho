1a100bbb4548a401ac0a7ee98a350e3321c23194
Matias Casteglione <matiascasteglione2002@gmail.com>
2024-05-01 23:52:53 -0300

    Delete commandkit.config.mjs

--- commandkit.config.mjs
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from 'commandkit';
- 
-export default defineConfig({
-  src: 'src', // The source directory of your project.
-  main: 'index.mjs', // The JavaScript entry point of your project.
-});
